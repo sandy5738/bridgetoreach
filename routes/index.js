ece5834b45899ae9b27882fc3a701de6f7aed8bf
Sandeep <sandeemnss@gmail.com>
2021-12-13 16:14:37 +0530

    add content and optimized images

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,22 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res)=>{
-    res.render('index');
+router.get("/", (req, res) => {
+  res.render("index");
 });
 
-router.get('/about',(req, res)=>{
-    res.render('about');
-})
+router.get("/about", (req, res) => {
+  res.render("about");
+});
 
-router.get('/contact',(req, res)=>{
-    res.render('contact');
-})
+router.get("/contact", (req, res) => {
+  res.render("contact");
+});
+router.get("/donate", (req, res) => {
+  res.render("donate");
+});
+router.get("/join", (req, res) => {
+  res.render("joinUs");
+});
 
 module.exports = router;
